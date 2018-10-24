94852f923cbec827129000e56bb13ac5c5caabe2
Lukas Rauchenzauner <rauchi13.lr@gmail.com>
2018-10-24 15:44:55 +0200

    Converter 1.0  Everything Fine

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log('Not implemented yet!');
+const convert = require('convert-units');
+try {
+    if (process.argv.length == 6 && process.argv[4] == 'to' && !isNaN(process.argv[2])) {
+        console.log(process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + " " + process.argv[5]);
+    } else
+        console.log('Invalid parameters');
+        
+} catch (e) {
+    console.log('Invalid parameters');
+}
